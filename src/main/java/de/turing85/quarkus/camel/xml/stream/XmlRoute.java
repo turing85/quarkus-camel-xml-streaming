801627119901469ce517ddd681349115b8f973dd
Marco Bungart <marco.bungart@googlemail.com>
2024-08-27 23:35:30 +0200

    Set the response content encoding from the accept header

--- src/main/java/de/turing85/quarkus/camel/xml/stream/XmlRoute.java
+++ src/main/java/de/turing85/quarkus/camel/xml/stream/XmlRoute.java
@@ -1,7 +1,10 @@
 package de.turing85.quarkus.camel.xml.stream;
 
+import java.nio.charset.Charset;
 import java.util.Map;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import jakarta.inject.Singleton;
@@ -11,6 +14,7 @@ import de.turing85.quarkus.camel.xml.stream.processor.xml.XmlProcessor;
 import io.vertx.core.http.HttpMethod;
 import lombok.AllArgsConstructor;
 import org.apache.camel.Exchange;
+import org.apache.camel.Message;
 import org.apache.camel.builder.RouteBuilder;
 
 import static org.apache.camel.builder.endpoint.StaticEndpointBuilders.platformHttp;
@@ -18,21 +22,53 @@ import static org.apache.camel.builder.endpoint.StaticEndpointBuilders.platformH
 @Singleton
 @AllArgsConstructor
 public class XmlRoute extends RouteBuilder {
+  public static final String ENCODING_GROUP_NAME = "encoding";
+  private static final Pattern ENCODING_EXTRACTOR =
+      Pattern.compile("^.*;charset=(?<%s>.*?)(?:;.*)?$".formatted(ENCODING_GROUP_NAME));
+
   private final XmlProcessor processor;
 
   @Override
   public void configure() {
     // @formatter:off
-    from(platformHttp("/xml")
-        .httpMethodRestrict(HttpMethod.POST.name())
-        .consumes(MediaType.APPLICATION_XML)
-        .produces(MediaType.APPLICATION_XML))
+    from(
+        platformHttp("/xml")
+            .httpMethodRestrict(HttpMethod.POST.name())
+            .consumes(MediaType.APPLICATION_XML + ";charset=ISO-8859-15")
+            .produces(MediaType.APPLICATION_XML))
+        .log("headers: ${headers}")
         .setProperty(XmlProcessor.PROPERTY_NAME_ADDITIONAL_VALUES_TO_EXTRACT,
             constant(Set.of("bang", "bongo")))
         .process(processor)
-        .process(XmlRoute::constructBody)
+        .process(XmlRoute::constructResponse)
     ;
     // @formatter:on
+  }
+
+  private static void constructResponse(Exchange exchange) {
+    constructBody(exchange);
+    configureResponseContentType(exchange);
+  }
+
+  private static void configureResponseContentType(Exchange exchange) {
+    String accept = exchange.getIn().getHeader("Accept", String.class);
+    Matcher matcher = ENCODING_EXTRACTOR.matcher(accept);
+    if (matcher.matches()) {
+      setCharset(exchange, matcher.group(ENCODING_GROUP_NAME));
+    }
+  }
+
+  private static void setCharset(Exchange exchange, String charset) {
+    Message in = exchange.getIn();
+    // @formatter:off
+    in.setHeaders(Map.of(
+        Exchange.HTTP_CHARACTER_ENCODING,
+        charset,
+
+        Exchange.CONTENT_TYPE,
+        "%s;charset=%s".formatted(in.getHeader(Exchange.CONTENT_TYPE, String.class), charset)));
+    // @formatter:on
+    in.setBody(in.getBody(String.class).getBytes(Charset.forName(charset)));
   }
 
   private static void constructBody(Exchange exchange) {
